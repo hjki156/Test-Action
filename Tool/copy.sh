bd14ba23e9b975851e78d749bc55ce4860796e9e
tobeko <huzihan@111.com>
2025-01-16 19:37:22 +0800

    Fixed

--- Tool/copy.sh
+++ Tool/copy.sh
@@ -8,16 +8,3 @@ if [! -d "$target_dir" ]; then
 fi
 
 find "$source_dir" -type f -exec cp {} "$target_dir" \;
-
-file_count=1
-for file in "$target_dir"/*; do
-  base_name=$(basename "$file")
-  if [ -e "$target_dir/$base_name" ]; then
-    new_name="$target_dir/${base_name%.*}_$file_count.${base_name##*.}"
-    while [ -e "$new_name" ]; do
-      ((file_count++))
-      new_name="$target_dir/${base_name%.*}_$file_count.${base_name##*.}"
-    done
-    mv "$file" "$new_name"
-  fi
-done
